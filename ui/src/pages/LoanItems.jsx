2dc317a3f3ed67189c86f283626bb88a748e07f6
Bas Buvelot <s.f.buvelot@gmail.com>
2019-05-28 11:34:37 +0200

    responsiveness improved

--- ui/src/pages/LoanItems.jsx
+++ ui/src/pages/LoanItems.jsx
@@ -1,120 +1,114 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import { Box, Text, Image, Button, Grid } from 'grommet';
+import { Box, Text, Image, Button, Grid } from "grommet";
 
+import products from "../helpers/products.json";
 
-import products from '../helpers/products.json'
-
-
-import * as cookie from '../helpers/cookie.js';
+import * as cookie from "../helpers/cookie.js";
 
 class LoanItems extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			products: []
-		}
-	}
-	// constructor(props) {
-	// 	super(props)
-	// 	this.state = {
-	// 		username: JSON.parse(cookie.get('username'))
-	// 	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+  // constructor(props) {
+  // 	super(props)
+  // 	this.state = {
+  // 		username: JSON.parse(cookie.get('username'))
+  // 	}
 
-	// 	// username = JSON.parse(cookie.get('username'));
+  // 	// username = JSON.parse(cookie.get('username'));
 
-	// 	cookie.set('username', '[]')
-	// }
+  // 	cookie.set('username', '[]')
+  // }
 
-	// function ActionLink(){
-	// 	function handleClick(e){
-	// 		e.preventDefault();
-	// 		console.log('The link is clickable.');
-	// 	}
-	// }
+  // function ActionLink(){
+  // 	function handleClick(e){
+  // 		e.preventDefault();
+  // 		console.log('The link is clickable.');
+  // 	}
+  // }
 
-	// componentDidMount() {
-	// 	fetch("C:/repositories/DeptML-service/resources/products.csv")
-	// 		.then(res => res.json())
-	// 		.then(json => {
-	// 			this.setState({
-	// 				products: json
-	// 			})
-	// 		});
-	// 	console.log(this.state.products)
-	// }
+  // componentDidMount() {
+  // 	fetch("C:/repositories/DeptML-service/resources/products.csv")
+  // 		.then(res => res.json())
+  // 		.then(json => {
+  // 			this.setState({
+  // 				products: json
+  // 			})
+  // 		});
+  // 	console.log(this.state.products)
+  // }
 
-
-
-
-	render() {
-		return (
-			<Box
-				direction="column"
-				align="center"
-				justify="between"
-				pad={{ horizontal: "medium", vertical: "small" }}
-				gap="large"
-				background="light-2"
-			>
-				{/* ItemCard */}
-				{/* {products.map(product => (
+  render() {
+    return (
+      <Box
+        direction="column"
+        align="center"
+        justify="between"
+        pad={{ horizontal: "medium", vertical: "small" }}
+        gap="large"
+        background="light-2"
+        responsive={true}
+      >
+        {/* ItemCard */}
+        {/* {products.map(product => (
 					<Itemcard></Itemcard>
 				))}
 
 				<Text>Please click <Link to="/">here</Link> to select a user</Text> */}
 
-				<Grid
-					columns={{
-						count: 3,
-						size: "auto"
-					}}
-					gap="small"
-					>
-
-					{products.map(product => (
-						<Itemcard></Itemcard>
-						))}
-				</Grid>
-
-			</Box>
-		)
-	}
+        <Grid
+          columns={{
+            count: 3,
+            size: "small"
+          }}
+          rows={{
+            count: 3,
+            size: "flex"
+          }}
+          gap="small"
+        >
+          {products.map(product => (
+            <Itemcard />
+          ))}
+        </Grid>
+      </Box>
+    );
+  }
 }
 
 const Itemcard = () => {
-	return (
+  return (
+    <Box
+      direction="column"
+      align="start"
+      justify="start"
+      width="medium"
+      pad={{ horizontal: "small", vertical: "small" }}
+      background="dark-2"
+      responsive={true}
+    >
+      <Box width="medium" height="small">
+        <Image
+          fit="cover"
+          size="flex"
+          src="https://assets.razerzone.com/eeimages/products/26727/razer-blade-hero-laptop-v3.png"
+        />
+      </Box>
 
-		<Box
-			direction="column"
-			align="start"
-			justify="start"
-			width="medium"
-			pad={{ horizontal: "small", vertical: "small" }}
-			background="dark-2">
-			<Box
-				width="medium"
-				height="small"
-			>
-				<Image
-					fit="cover"
-					src="https://assets.razerzone.com/eeimages/products/26727/razer-blade-hero-laptop-v3.png"
-				/>
-			</Box>
-
-			<Text>Item name</Text>
-			<Text
-				size="small"
-			>
-				Match rating
-	</Text>
-			<Button
-				label="Click"
-				onClick={() => alert('Doesn\'t work yet')}
-			/>
-		</Box>
-	)
-}
+      <Text size="flex">Item name</Text>
+      <Text size="xsmall">Match rating</Text>
+      <Button
+        label="Click"
+        size="flex"
+        onClick={() => alert("Doesn't work yet")}
+      />
+    </Box>
+  );
+};
 
 export default LoanItems;
