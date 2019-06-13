4459ddbf393cddba9b7b59bc9eee27f1525df5c2
Hicham <hichagzanay@gmail.com>
2019-06-13 15:22:39 +0200

    Algorithm changes
    Algorithm is now a callable function that takes an int FunctionGroup and returns athe prediction for that functiongroup. Data splitting is also done automatically by the algorithm itself instead of having to be done manually.

--- service/testpredict.py
+++ service/testpredict.py
@@ -7,107 +7,116 @@ from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 import pickle
 import operator
 
-# Importing dataset
-training_Data = pd.read_csv("../resources/trainingdata2/Training_Function5_#3.csv")
 
-testing_Data = pd.read_csv("../resources/testdata2/Testing_Function5_#3.csv")
+def Naive_Bayes(FunctionGroup):
+    # Importing dataset
+    Dataset = pd.read_csv(f"../resources/unsplitdataV2/Dept_History_Function{FunctionGroup}.csv")
 
-# Convert categorical variable to numeric
-#data["Typecleaned"]=np.where(data["Type"]=="Laptop",0,1)
+    #training_Data = pd.read_csv("../resources/trainingdata2/Training_Function5_#3.csv")
 
-# Cleaning training dataset of NaN
-training_Data_Clean = training_Data[[
-    #-----
-    "PurchaseID",
-    "EmployeeID",
-    "LoanItemID"
-]].dropna(axis=0, how='any')
+    #testing_Data = pd.read_csv("../resources/testdata2/Testing_Function5_#3.csv")
 
-# Cleaning testing dataset of NaN
-testing_Data_Clean = testing_Data[[
-    #-----
-    "PurchaseID",
-    "EmployeeID",
-    "LoanItemID"
-]].dropna(axis=0, how='any')
+    #make datasplit
+    train, test = train_test_split(Dataset, test_size=0.2)
 
-# Split dataset in training and test datasets
-#X_train, X_test = train_test_split(data, test_size=0.5, random_state=int(time.time()))
+    # Convert categorical variable to numeric
+    #data["Typecleaned"]=np.where(data["Type"]=="Laptop",0,1)
 
-# Instantiate the classifier
-gnb = GaussianNB()
-used_features =[
-    #-----
-    #"PurchaseID"
-   # "EmployeeID",
-    "LoanItemID",
-    #"ID",
-    #"Brand",
-    #"CPUrating",
-    #"RAM",
-    #"GPUrating"	
+    # Cleaning training dataset of NaN
+    training_Data_Clean = train[[
+        #-----
+        "PurchaseID",
+        "EmployeeID",
+        "LoanItemID"
+    ]].dropna(axis=0, how='any')
 
-]
+    # Cleaning testing dataset of NaN
+    testing_Data_Clean = test[[
+        #-----
+        "PurchaseID",
+        "EmployeeID",
+        "LoanItemID"
+    ]].dropna(axis=0, how='any')
 
-# Train classifier
-gnb.fit(
-    training_Data_Clean[used_features].values,
-    training_Data_Clean["LoanItemID"]
-)
+    # Split dataset in training and test datasets
+    #X_train, X_test = train_test_split(data, test_size=0.5, random_state=int(time.time()))
 
-#prediction algorithm testing data
-item_Predictions = gnb.predict(testing_Data[used_features])
+    # Instantiate the classifier
+    gnb = GaussianNB()
+    used_features =[
+        #-----
+        #"PurchaseID"
+       # "EmployeeID",
+        "LoanItemID",
+        #"ID",
+        #"Brand",
+        #"CPUrating",
+        #"RAM",
+        #"GPUrating"	
 
-# Print results
-print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%"
-      .format(
-          testing_Data_Clean.shape[0],
-          (testing_Data_Clean["LoanItemID"] != item_Predictions).sum(),
-          100*(1-(testing_Data_Clean["LoanItemID"] != item_Predictions).sum()/testing_Data_Clean.shape[0])
-))
+    ]
 
+    # Train classifier
+    gnb.fit(
+        training_Data_Clean[used_features].values,
+        training_Data_Clean["LoanItemID"]
+    )
 
+    #prediction algorithm testing data
+    item_Predictions = gnb.predict(test[used_features])
 
-
-
-
-#- ??what are these datasets used for??
-
-## Importing dataset2
-#data2 = pd.read_csv("../resources/products.csv")
-#data2.rename(columns={'ID':'LoanItemID'}, inplace=True)
-
-## Importing dataset3
-## Convert categorical variable to numeric
-##ata["Typecleaned"]=np.where(data["Type"]=="Laptop",0,1)
-
-##importing dataset3
-#data3 = pd.read_csv("../resources/profiles.csv")
-#data3.rename(columns={'Id':'LoanItemID'}, inplace=True)
+    print(test)
+    # Print results
+    print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%"
+          .format(
+              testing_Data_Clean.shape[0],
+              (testing_Data_Clean["LoanItemID"] != item_Predictions).sum(),
+              100*(1-(testing_Data_Clean["LoanItemID"] != item_Predictions).sum()/testing_Data_Clean.shape[0])
+    ))
 
 
-testing = gnb.predict(testing_Data_Clean[used_features])
-
-def prediction():
-    array = gnb.predict(testing_Data_Clean[used_features]).tolist()
-
-    rating = {}
-    for item in array:
-        occur = array.count(item)
-        rating[item] = int(occur)
-
-    return rating
 
 
-#??what test.pickle and wb and rb??
-file = open('test.pickle', 'wb')
-pickle.dump(testing,file)
-file.close()
-file = open('test.pickle' ,'rb')
-#openn = pickle.load(file)
-
-prodlist = testing.tolist()
-
-print(prodlist)
 
 
+    #- ??what are these datasets used for??
+
+    ## Importing dataset2
+    #data2 = pd.read_csv("../resources/products.csv")
+    #data2.rename(columns={'ID':'LoanItemID'}, inplace=True)
+
+    ## Importing dataset3
+    ## Convert categorical variable to numeric
+    ##ata["Typecleaned"]=np.where(data["Type"]=="Laptop",0,1)
+
+    ##importing dataset3
+    #data3 = pd.read_csv("../resources/profiles.csv")
+    #data3.rename(columns={'Id':'LoanItemID'}, inplace=True)
+
+
+    testing = gnb.predict(testing_Data_Clean[used_features])
+
+    def prediction():
+        array = gnb.predict(testing_Data_Clean[used_features]).tolist()
+
+        rating = {}
+        for item in array:
+            occur = array.count(item)
+            rating[item] = int(occur)
+
+        return rating
+
+
+    #??what test.pickle and wb and rb??
+    file = open('test.pickle', 'wb')
+    pickle.dump(testing,file)
+    file.close()
+    file = open('test.pickle' ,'rb')
+    #openn = pickle.load(file)
+
+    prodlist = testing.tolist()
+
+    print(prodlist)
+
+
+Naive_Bayes(2)
